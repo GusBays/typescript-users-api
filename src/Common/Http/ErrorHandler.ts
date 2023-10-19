97a4bbf0929629cf604b8a7a9ab88cf84bda98b0
Gustavo Bays <bagy@192.168.1.10>
2023-10-19 17:25:45 -0300

    fix: removed console log

--- src/Common/Http/ErrorHandler.ts
+++ src/Common/Http/ErrorHandler.ts
@@ -3,7 +3,6 @@ import { ResponseTypeEnum } from './ResponseTypeEnum'
 
 export class ErrorHandler {
     handle(error: Error, res: Response): Response {
-        console.log(error)
         return res.status(ResponseTypeEnum.HTTP_INTERNAL_SERVER_ERROR).json({
             message: error.message
         })
